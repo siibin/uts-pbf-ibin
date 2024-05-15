f506b14b783dc3ab9f4ac10da8c5083b29b505ab
Kreasi Putra Subing <ralfian096@gmail.com>
2024-05-16 03:13:42 +0700

    Update welcome page

--- core/routes/web.php
+++ core/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('home', function () {
-    return view('welcome');
-});
-
 Route::get('/', function () {
-    return view('landing');
+    return view('welcome');
 });
